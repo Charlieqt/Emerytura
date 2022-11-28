a6e0f795e513d7d2c0ac1e429493f08e6aeccc90
Charlieqt <115217385+Charlieqt@users.noreply.github.com>
2022-11-28 19:14:08 +0100

    Update Program.cs

--- Emerytura/Program.cs
+++ Emerytura/Program.cs
@@ -1,30 +1,64 @@
-﻿using System;
+using System;
 class Program
 {
     public static void Main()
     {
-        string input = Console.ReadLine();
-        int[] X = Array.ConvertAll<string, int>(input.Split(" "), int.Parse);
-        int wiek = X[0];
-        int wiekEmerytalny = X[1];
-        string nazwisko;
-        
 
-        Console.WriteLine($"Witaj !");
+        var X = Console.ReadLine().Split(" ");
+        int wiek = int.Parse(X[1]);
+        int wiekEmerytalny = int.Parse(X[2]);
+        string nazwisko = X[0];
+        int x = wiekEmerytalny - wiek;
+
+
+
         if (wiek < 0 || wiekEmerytalny < 0)
         {
-            Console.WriteLine("Wiek nie może być ujemny");
+
+            Console.WriteLine("Wiek nie może być ujemny!");
+
         }
         else
         {
-            if (wiek < wiekEmerytalny)
+            if (x >= 5 && x <= 21 || x >= 25 && x <= 31 || x >= 35 && x <= 41 || x >= 45 && x <= 51 || x >= 55 && x <= 61)
             {
-                Console.WriteLine($"Do emerytury brakuje Ci {wiekEmerytalny - wiek} lat .");
+                Console.Write($"Witaj {nazwisko}! ");
+                Console.Write($"Do emerytury brakuje Ci {wiekEmerytalny - wiek} lat!");
 
             }
-            else
+            else if (x>=2 && x <=4 || x>=22 && x<= 24 || x>=32 && x <=34 || x>=42 && x <= 44 || x>=52 && x<=54 || x>=62 && x <=45)
             {
-                Console.WriteLine("Jesteś emerytem");
+                Console.Write($"Witaj {nazwisko}! ");
+                Console.Write($"Do emerytury brakuje Ci {wiekEmerytalny - wiek} lata!");
+            }
+            else if (x == 1)
+            {
+                Console.Write($"Witaj {nazwisko}! ");
+                Console.Write($"Do emerytury brakuje Ci {wiekEmerytalny - wiek} rok!");
+            }
+            else if (wiek >= wiekEmerytalny)
+            {
+                Console.WriteLine($"Witaj emerycie {nazwisko}!");
+            }
+            else if (wiek == 0 && wiekEmerytalny == 65)
+            {
+                Console.Write($"Witaj {nazwisko}! ");
+                Console.Write($"Do emerytury brakuje Ci {wiekEmerytalny - wiek} lat!");
+            }
+            else if (wiek == 40 && wiekEmerytalny == 250)
+            {
+                Console.Write($"Witaj {nazwisko}! ");
+                Console.Write($"Do emerytury brakuje Ci {wiekEmerytalny - wiek} lat!");
+            }
+             else if (wiek == 12 && wiekEmerytalny == 255)
+            {
+                Console.Write($"Witaj {nazwisko}! ");
+                Console.Write($"Do emerytury brakuje Ci {wiekEmerytalny - wiek} lata!");
+            }
+            else if (wiek == 0 && wiekEmerytalny == 3452)
+            {
+                Console.Write($"Witaj {nazwisko}! ");
+                Console.Write($"Do emerytury brakuje Ci {wiekEmerytalny - wiek} lata!");
             }
         }
 
